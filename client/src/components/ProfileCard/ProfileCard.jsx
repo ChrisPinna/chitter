2796a9f355f49e33583288da9c816f8ef5c649a7
christianpinna <christian.a.pinna@gmail.com>
2022-08-30 14:32:19 +0100

    Added ProfileCard component and css, Also imported css into ProfileSide

--- client/src/components/ProfileCard/ProfileCard.jsx
+++ client/src/components/ProfileCard/ProfileCard.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Cover from '../../images/cover.jpg'
+import Profile from '../../images/profileImg.jpg'
+import './ProfileCard.css'
 
 const ProfileCard = () => {
   return (
-    <div>ProfileCard</div>
+    <div className='ProfileCard'>
+      <div className="ProfileImages">
+        <img src={Cover} alt="" />
+        <img src={Profile} alt="" />
+      </div>
+
+      <div className="ProfileName">
+        <span>Christian Pinna</span>
+        <span>Developer</span>
+      </div>
+
+      <div className="followStatus">
+        <hr />
+        <div>
+          <div className="follow">
+            <span>6,890</span>
+            <span>Followers</span>
+          </div>
+          <div className="vl"></div>
+          <div className="follow">
+            <span>1</span>
+            <span>Following</span>
+          </div>
+        </div>
+        <hr />
+      </div>
+
+      <span>My Profile</span>
+      
+    </div>
   )
 }
 
