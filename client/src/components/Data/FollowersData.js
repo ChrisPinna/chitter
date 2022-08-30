f0b296676146f22da0ea6083e535dd888f8bf7c7
christianpinna <christian.a.pinna@gmail.com>
2022-08-30 23:16:55 +0100

    Added FlollowersCard component and css and added place holder Data to emulate backend Data

---
+++ client/src/components/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from "../../images/stanLee.jpg";
+import img2 from "../../images/hulk.jpg";
+import img3 from "../../images/thor.jpg";
+import img4 from "../../images/natasha.jpg";
+
+export const Followers = [
+  {name: "Stan Lee", username: "StanLee", img: img1},
+  {name: "Hulk", username: "Hulk", img: img2},
+  {name: "Thor", username: "Thor", img: img3},
+  {name: "Natasha", username: "Natasha", img: img4}
+];
