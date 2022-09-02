f43736fd07b73a65f3605043979b2209f4fe1039
christianpinna <christian.a.pinna@gmail.com>
2022-09-02 15:02:02 +0100

    Added feature to click on Photos button and upload a photo, get a preview on the page and be able to clear that photo by clicking on the cross

--- client/src/components/PostShare/PostShare.jsx
+++ client/src/components/PostShare/PostShare.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import profileImage from '../../images/profileImg.jpg'
 import './PostShare.css'
 import { UilScenery } from "@iconscout/react-unicons"
 import { UilPlayCircle } from "@iconscout/react-unicons"
 import { UilLocationPoint } from "@iconscout/react-unicons"
 import { UilSchedule } from "@iconscout/react-unicons"
+import { UilTimes } from "@iconscout/react-unicons"
 
 const PostShare = () => {
+    const [image, SetImage] = useState(null)
+    const imageRef = useRef()
+
+    const onImageChange = (event) => {
+        if(event.target.files && event.target.files[0]) {
+            let img = event.target.files[0]
+            SetImage({
+                image: URL.createObjectURL(img)
+            })
+        }
+    }
   return (
     <div className="PostShare">
         <img src={profileImage} alt="" />
         <div>
             <input type="text" placeholder="What's happening?"/>
             <div className="postOptions">
-                <div className="option" style={{color: "var(--photo)"}}>
+                <div className="option" style={{color: "var(--photo)"}} onClick={() => imageRef.current.click()}>
                     <UilScenery/>
                     Photo
                 </div>
@@ -32,7 +44,16 @@ const PostShare = () => {
                 <button className='button ps-button'>
                     Share
                 </button>
+                <div style={{display: "none"}}>
+                    <input type="file" name="myImage" ref={imageRef} onChange={onImageChange}/>
+                </div>
             </div>
+            {image && (
+                <div className="previewImage">
+                    <UilTimes onClick={() => SetImage(null)}/>
+                    <img src={image.image} alt="" />
+                </div>
+            )}
         </div>
     </div>
   )
