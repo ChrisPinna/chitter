4d5ec2a8e6b60a9aac323e787ae0bfbdcb0522b6
christianpinna <christian.a.pinna@gmail.com>
2022-09-08 11:27:32 +0100

    Added popup card component for the Share button

--- client/src/components/RightSide/RightSide.jsx
+++ client/src/components/RightSide/RightSide.jsx
@@ -5,9 +5,12 @@ import Home from '../../images/home.png'
 import Comment from '../../images/comment.png'
 import Notification from '../../images/noti.png'
 import { UilSetting } from '@iconscout/react-unicons'
+import { useState } from 'react'
+import ShareModal from '../ShareModel/ShareModel'
 
 
 const RightSide = () => {
+  const [modalOpened , setModalOpened] = useState(false)
   return (
     <div className="RightSide">
         <div className="navIcons">
@@ -17,9 +20,11 @@ const RightSide = () => {
             <img src={Comment} alt="" />
         </div>
         <TrendCard/>
-        <button className="button r-button">
+        <button className="button r-button" onClick={() => setModalOpened(true)}>
             Share
         </button>
+        <ShareModal modalOpened={modalOpened}
+            setModalOpened={setModalOpened}/>
     </div>
   )
 }
