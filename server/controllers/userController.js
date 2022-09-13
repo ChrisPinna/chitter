9dd448dd6f6d9575d118fe85cb1b4572a1a1cc37
christianpinna <christian.a.pinna@gmail.com>
2022-09-13 14:30:51 +0100

    Added getUser api call to userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,8 +7,10 @@ export const getUser = async (req, res) => {
         const user = await UserModel.findById(id);
         
         if (user) {
-            res.status(200).json(user);
-
+            const { password, ...otherDetails } = user._doc
+            res.status(200).json(otherDetails);
+        } else {
+            res.status(404).json("User does not exist!")
         }
     } catch (error) {
         res.status(500).json({message: error.message})
