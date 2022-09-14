8cf702e2c9f4135f818fbb34c045efdbdae91e1a
christianpinna <christian.a.pinna@gmail.com>
2022-09-14 23:04:56 +0100

    Added user register, login, update and delete to controller and rutes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,13 @@
 import UserModel from "../models/userModel.js";
+import bcrypt from "bcrypt"
 
 // get a User
 export const getUser = async (req, res) => {
+
     const id = req.params.id;
+
     try {
+
         const user = await UserModel.findById(id);
         
         if (user) {
@@ -12,7 +16,58 @@ export const getUser = async (req, res) => {
         } else {
             res.status(404).json("User does not exist!")
         }
+
     } catch (error) {
         res.status(500).json({message: error.message})
     }
+};
+
+// update a user
+export const updateUser = async (req, res) => {
+
+    const id = req.params.id;
+    const { currentUserId, currentUserAdminStatus, password} = req.body;
+
+    if ( id === currentUserId || (currentUserAdminStatus === true)) {
+
+        try {
+
+            if (password) {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(password, salt);
+            }
+
+            const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true});
+            res.status(200).json(user);  
+
+        } catch (error) {
+            res.status(500).json(error)
+        };
+
+    } else {
+        res.status(403).json("Access Denied! No admin status detected");
+    };
+
+};
+
+// Delete user
+export const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    const {currentUserId, currentUserAdminStatus} = req.body;
+
+    if ( id === currentUserId || (currentUserAdminStatus === true) ) {
+
+        try {
+
+            await UserModel.findByIdAndDelete(id);
+            res.status(200).json("User deleted successfully")
+        } catch (error) {
+            res.status(500).json(error)
+        }
+
+    } else {
+
+        res.status(500).json("Access Denied! No admin status detected")
+    };
+
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getUser } from "../controllers/userController.js";
+import { deleteUser, getUser, updateUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get('/:id', getUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
