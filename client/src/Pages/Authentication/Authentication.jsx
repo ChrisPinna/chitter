dcef6d8ac5c15a378ad55ec81c69ad10aa06588f
christianpinna <christian.a.pinna@gmail.com>
2022-09-27 17:19:38 +0100

    Added conditional rendering to deal with Sign up or Log in

--- client/src/Pages/Authentication/Authentication.jsx
+++ client/src/Pages/Authentication/Authentication.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Authentication.css'
 import LogoBig from '../../images/LogoBig.png'
 
 const Authentication = () => {
+    const [isSignUp, setIsSignUp] = useState(false)
+
   return (
     <div className="Authentication">
+        {/* Left Side */}
         <div className="a-left">
             <img src={LogoBig} alt="" />
             <div className="Webname">
                 <h1>Chitter</h1>
-                <h6>Explore the ideas throughout the world</h6>
+                <h6>Interct with other peoples ideas!</h6>
             </div>
         </div>
+        {/* Right Side */}
+        <div className="a-right">
+            <form action="" className="infoForm AuthenticationForm">
+                <h3>{isSignUp ? "Sign up" : "Log in"}</h3>
 
-        <LogIn/>
+                {isSignUp && 
+                    <div>
+                        <input type="text" placeholder='First Name' className='infoInput' name='firstname'/>
+                        <input type="text" placeholder='Last Name' className='infoInput' name='lastname'/>
+                    </div>
+                }
+                
+                <div>
+                    <input type="text" placeholder='Username' className='infoInput' name='username'/>
+                </div>
+                <div>
+                    <input type="text" placeholder='Password' className='infoInput' name='password'/>
+                    {isSignUp && <input type="text" placeholder='Confirm Password' className='infoInput' name='confirmpassword'/>}
+                </div>
+                <div>
+                    <span onClick={() => setIsSignUp(!isSignUp)} style={{fontSize: '12px', cursor: "pointer"}}> {isSignUp ? 'Already have an account? Log in!' : "Don't have and account? Sign up!"}</span>
+                </div>
+                <button className="button infoButton" type='submit'>
+                    {isSignUp ? 'Sign up' : 'Log in'}
+                </button>
+                
+            </form>
+        </div>
     </div>
   )
 }
